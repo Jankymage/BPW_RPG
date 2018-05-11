419bb7296c18603e652a3aa81edee6057ed73dd5
Jankymage <36413963+Jankymage@users.noreply.github.com>
2018-05-11 21:26:02 +0200

    quest eindig gemaakt

--- Assets/Script/Quest/QuestFetch.cs
+++ Assets/Script/Quest/QuestFetch.cs
@@ -12,26 +12,38 @@ public class QuestFetch : MonoBehaviour {
 	
 	//TODO:
 	//quest inleveren.
-	//tracker > fixen dat hij goed update
 
 	//voor het tellen van de objecten.
 	private int count = 0;
 	public int countMax = 10;
 	public Text questText;
 	private Text questTextUpdate;
+	private Text questComplete;
 	public GameObject canvas; //voor plaatsing questText
 
 	// Use this for initialization
 	void Start () {
+
 		count  = 0;
-		Debug.Log("quest actief");
+		
+		//maakt een text aan om de status van de quest bij te houden en zet deze op 0
 		questTextUpdate = Instantiate(questText, canvas.transform);
 		SetCountText();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//voor weergeven huidige status telling
+
+		//als de quest is afgerond
+		if(count >= countMax && !questComplete){
+
+			//vermoord de quest update text
+			Destroy(questTextUpdate);
+
+			//maak een nieuwe text aan (bestaan van deze text werkt ook als bool)
+			questComplete = Instantiate(questText, canvas.transform);
+			questComplete.text = "Quest Complete!";
+		}
 		 
 	}
 
@@ -40,18 +52,13 @@ public class QuestFetch : MonoBehaviour {
 		//zorgt dat dit deel niet word uitgevoerd als het script niet enabled is.
 		if (!enabled) return;
 
-		//als er een collectable gepakt word, gaat de count omhoog
-		if(other.CompareTag("Collectable")){
+		//als er een collectable gepakt word en de quest nog niet afgerond is,
+		//gaat de count omhoog
+		if(other.CompareTag("Collectable") && !questComplete){
 			count += 1;
 			other.gameObject.SetActive(false);
 			
 			SetCountText();
-			
-			Debug.Log(count);
-		}
-
-		if(count >= countMax){
-			Debug.Log("quest complete!");
 		}
 		
 	}
