b11a4231040c252254e9edaeaa5991bb5f99bf20
Jankymage <36413963+Jankymage@users.noreply.github.com>
2018-05-19 19:04:46 +0200

    Targeting (positie en afstand)

--- Assets/Script/Targeting.cs
+++ Assets/Script/Targeting.cs
@@ -6,18 +6,21 @@ public class Targeting : MonoBehaviour {
 
 //script voor clickable targeting
 
-
-//TODO:
-//alleen als in buurt is
-//BOVEN target spawnen (parenting?)
-//actief/deactief (dood of in buurt)
-//
-
 	//voor visuele weergave target
-	public Transform targetIndicate;
+	public GameObject targetIndicate;
 
+	//om oude targets goed te verwerken
 	private GameObject targetOld;
 	private GameObject targetNew;
+
+	//afstand voor selectie bijhouden
+	private float distance;
+	public float maxDistance = 15;
+
+	//om afstand van huidige target bij te houden.
+	private bool targeted = false;
+	private float targetedDistance;
+	public float targetedDistanceMax = 25;
 
 	// Use this for initialization
 	void Start () {
@@ -26,6 +29,24 @@ public class Targeting : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		//kijkt of de targeted bool true is (wat betekend dat er een target is)
+		if(targeted){
+
+			//kijkt wat de afstand tussen de player en de target is.
+			targetedDistance = Vector3.Distance(targetNew.transform.position, transform.position);
+
+			//als de afstand te groot is word alle target informat gereset en de targetIndicate gedeactiveerd.
+			if(targetedDistance > targetedDistanceMax){
+				targetNew = null;
+				targetOld = null;
+				targetIndicate.transform.parent = null;
+				targetIndicate.SetActive(false);
+				targeted = false;
+
+			}
+
+		}
 
 		//als linker muis geklikt word
 		if(Input.GetMouseButtonDown(0)){
@@ -38,19 +59,35 @@ public class Targeting : MonoBehaviour {
 			if (Physics.Raycast(ray, out hitInfo))
 			{
 
-					if (hitInfo.collider.gameObject.tag == "Target")
-					{
+				if (hitInfo.collider.gameObject.tag == "Target")
+				{
 
-					Debug.Log(hitInfo.collider.gameObject);
+				Debug.Log(hitInfo.collider.gameObject);
 
-					targetNew = hitInfo.collider.gameObject;
+				targetNew = hitInfo.collider.gameObject;
 
 					//als er een nieuwe target is
 					if(targetOld != targetNew){
 						targetOld = targetNew;
 
-						//plaatst de targetIndicator boven de target.
-						targetIndicate.position = (targetNew.transform.position);
+						//bekijkt afstand tussen target en speler
+						distance = Vector3.Distance(targetNew.transform.position, transform.position);
+
+						if(distance < maxDistance){
+
+						//plaatst de targetIndicator boven de target en parent die.
+						targetIndicate.transform.position = new Vector3(targetNew.transform.position.x, targetNew.transform.position.y * 3, targetNew.transform.position.z);
+						targetIndicate.transform.SetParent(targetNew.transform);
+						
+						//zet de targetIndicate op actief
+						targetIndicate.SetActive(true);
+
+						//zorgt dat de targeted bool true is.
+						targeted = true;
+
+						}
+
+						
 
 					}
 
